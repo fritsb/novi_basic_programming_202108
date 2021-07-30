bbfe3402750b1aae0eaa599eecce2816935f9907
Frits <frits@streeksoft.nl>
2021-07-30 16:30:13 +0200

    Extra comments

--- StudentGradeApp/src/nl/streeksoft/novi/Main.java
+++ StudentGradeApp/src/nl/streeksoft/novi/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
         // Hieronder een while-lus die net zo lang blijft draaien zolang de boolean isRunning true is. isRunning wordt op false gezet op regel 57 als de gebruiker "NEE" opgeeft. 
         while(isRunning) {
+            // We gebruiken het Scanner object om gegevens door de gebruiker te laten invoeren. 
             Scanner input = new Scanner(System.in);
             System.out.println("Geef naam op van de student:");
             studentName = input.nextLine(); 
@@ -21,9 +22,10 @@ public class Main {
             input.nextLine(); // Extra nextLine() omdat we anders een probleem krijgen op regel 54
             System.out.println("De student " + studentName + " heeft als cijfer: " + grade);
 
-
+            // Keuze-constructie om te controleren of het cijfer is tussen 6 en 10.  
             if (grade >= 6 && grade <= 10) {
                 System.out.println("Het cijfer is een voldoende! Gefeliciteerd!");
+                // Switch-statement om te controleren of een cijfer gelijk is aan een bepaald getal. 
                 switch(grade) {
                     case 10:
                         System.out.println("Een 10, PERFECT! ");
@@ -44,14 +46,12 @@ public class Main {
                         System.out.println("Dit komt niet voor.");
                         break;
                 }
-
-
-            } else if (grade > 1 && grade < 6) {
+            } else if (grade > 1 && grade < 6) { // als het cijfer onvoldoende is
                 System.out.println("Het cijfer is helaas onvoldoende. Volgende keer beter! ");
-            } else {
+            } else { // andere scenarios (verkeerde invoer). 
                 System.out.println("Er is iets verkeerd gegaan. Probeer opnieuw het cijfer in te voeren.");
             }
-
+            // Wanneer men NEE invoert, wordt de while-lus gestopt; daardoor ook de applicatie. 
             System.out.println("Wil je nog een cijfer invoeren? Type JA of NEE");
             String answer = input.nextLine();
             boolean answerResult = answer.equals("NEE");
