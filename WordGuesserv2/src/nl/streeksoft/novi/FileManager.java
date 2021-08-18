c9a314b2ffdc2922c75b72380c88c980c32c14e0
Frits <frits@streeksoft.nl>
2021-08-18 19:01:00 +0200

    Added extra writeData-method

--- WordGuesserv2/src/nl/streeksoft/novi/FileManager.java
+++ WordGuesserv2/src/nl/streeksoft/novi/FileManager.java
@@ -5,6 +5,22 @@ import java.io.FileWriter;
 import java.util.Scanner;
 
 public class FileManager {
+    // Algemene writeData methode. Deze werkt overal. Je roept het aan als volgt:
+    // FileManager fm = new FileManager();
+    // fm.writeData("tekst die naar het bestand moet");
+     public void writeData(String data) {
+        try {
+            File myFile = new File("data.txt");
+            FileWriter fw = new FileWriter(myFile, true);
+            fw.write(data + "\n");
+            fw.close();
+        }
+        catch (Exception ex) {
+            System.out.println("Er is iets verkeerd gegaan!");
+        }
+    }
+        
+    // Onderstaande writeData methode werkt alleen met de bestanden van WordGuesserv2
     public void writeData(String word, String userWord) {
         CompareWords cw = new CompareWords();
         try {
@@ -14,6 +30,7 @@ public class FileManager {
                 fw.write("Woord: " + word + " is correct geraden" + "\n");
                 fw.close();
             }
+            
         }
         catch (Exception ex) {
             System.out.println("Er is iets verkeerd gegaan!");
